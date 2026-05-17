b869eea10adef116171b5f2f99a52f01db2cd1e4
umerkh13280-beep <umerkh13280@gmail.com>
2026-05-17 13:01:22 +0500

    Reset API URL to localhost

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://taskflow-bay-xi.vercel.app/api'
+  baseURL: 'http://localhost:5000/api'
 });
 
 API.interceptors.request.use((req) => {
